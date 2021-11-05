09a57ccb9d5bb34902980dcaabd9515520d13374
Adrian Carpenter <adriatic.c@gmail.com>
2021-11-04 21:43:00 -0700

    State dump

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,86 @@
 <template>
   <div v-if="ready" id="deadline">
-    <div class="header">{{ carbon ? carbon.labels[0] : "" }}</div>
-    <div class="side">
-      <div v-for="c in 'DEADLINE'.slice()" :key="c">{{ c }}</div>
-    </div>
-    <div class="group">
-      <div class="value">{{ countdown.years }}</div>
-      <div class="label">Year{{ countdown.years > 1 ? "s" : " " }}</div>
-    </div>
-    <div class="group">
-      <div class="value">{{ countdown.days }}</div>
-      <div class="label">Day{{ countdown.days > 1 ? "s" : " " }}</div>
-    </div>
-    <div class="group">
-      <div class="value">{{ countdown.hours }}</div>
-      <div class="label">:</div>
-      <div class="value">{{ countdown.minutes }}</div>
-      <div class="label">:</div>
-      <div class="value">{{ countdown.seconds }}</div>
+    <div class="module">
+      <div class="header">
+        <div class="tab">DEADLINE</div>
+        {{ carbon.labels[0] }}
+      </div>
+      <div class="groups">
+        <div class="group">
+          <div class="value">{{ deadlineValue.years }}</div>
+          <div class="label">Year{{ deadlineValue.years > 1 ? "s" : " " }}</div>
+        </div>
+        <div class="group">
+          <div class="value">{{ deadlineValue.days }}</div>
+          <div class="label">Day{{ deadlineValue.days > 1 ? "s" : " " }}</div>
+        </div>
+        <div class="group">
+          <div class="value">{{ deadlineValue.hours }}</div>
+          <div class="label">:</div>
+          <div class="value">{{ deadlineValue.minutes }}</div>
+          <div class="label">:</div>
+          <div class="value">{{ deadlineValue.seconds }}</div>
+        </div>
+      </div>
     </div>
   </div>
-  <div v-if="ready" id="lifeline">
-    <div class="side">
-      <div v-for="c in 'LIFELINE'.slice()" :key="c">{{ c }}</div>
+  <div v-if="ready" id="lifelines">
+    <div class="module">
+      <div class="header">
+        <div class="tab">LIFELINE</div>
+        {{ renewables.labels[0] }}
+      </div>
+      <div class="groups">
+        <div class="group">
+          <div class="value">{{ renewableValue }}</div>
+        </div>
+        <div class="group">
+          <div class="label">{{ renewables.unit_labels[0] }}</div>
+        </div>
+      </div>
     </div>
-    <div class="group">
-      <div class="value">{{ renewablePercent }}</div>
-      <div class="label">Year{{ countdown.years > 1 ? "s" : " " }}</div>
+    <div class="module">
+      <div class="header">
+        <div class="tab">LIFELINE</div>
+        {{ gcf.labels[0] }}
+      </div>
+      <div class="groups">
+        <div class="group">
+          <div class="value">${{ gcfValue }}</div>
+        </div>
+        <div class="group">
+          <div class="label">{{ gcf.unit_labels[0].replace("$", "") }}</div>
+        </div>
+      </div>
     </div>
-    <div class="group">
-      <div class="value">{{ renewablePercent }}</div>
-      <div class="label">Year{{ countdown.years > 1 ? "s" : " " }}</div>
+    <div class="module">
+      <div class="header">
+        <div class="tab">LIFELINE</div>
+        {{ gcf.labels[0] }}
+      </div>
+      <div class="groups">
+        <div class="group">
+          <div class="value">{{ indieValue }}</div>
+        </div>
+        <div class="group">
+          <div class="label">{{ indie.unit_labels[0] }}</div>
+        </div>
+      </div>
     </div>
+  </div>
+  <div v-if="ready" id="newsfeed">
+    <div class="news" one :style="animationDuration">{{ feedText }}</div>
+    <div class="news" two :style="animationDuration">{{ feedText }}</div>
   </div>
   <div id="branding" @click="handleClick">
     <img src="./climateclock.svg" />
-    <div>
-      <h1>ClimateClock</h1>
-      <h4>©{{ new Date().getFullYear() }} ClimateClock.world</h4>
-    </div>
   </div>
 </template>
 
 <script>
 import { DateTime } from "luxon"
 
-const TICK_INTERVAL = 250
+const TICK_INTERVAL = 100
 
 export default {
   data: () => ({
@@ -58,11 +93,12 @@ export default {
     newsfeed: {},
     carbon: {},
     renewables: {},
+    indie: {},
     gcf: {},
     open: window.open,
   }),
   computed: {
-    countdown() {
+    deadlineValue() {
       if (!(this.deadline && this.now)) return {}
       let delta = this.deadline.diff(this.now, [
         "years",
@@ -79,7 +115,7 @@ export default {
         seconds: this.pad(delta.get("seconds")),
       }
     },
-    renewablePercent() {
+    renewableValue() {
       let tElapsed = this.now - new Date(this.renewables.timestamp).getTime()
       return (
         this.renewables.initial +
@@ -110,7 +146,6 @@ export default {
       }
     },
 
-    // Items below are skin/theme-specific
     animationDuration() {
       return { animationDuration: 0.15 * this.feedText.length + "s" }
     },
@@ -137,9 +172,9 @@ export default {
         let modules = data.data.modules
         this.carbon = modules.carbon_deadline_1
         this.renewables = modules.renewables_1
-        this.newsfeed = modules.newsfeed_1
         this.gcf = modules.green_climate_fund_1
         this.indie = modules.indigenous_land_1
+        this.newsfeed = modules.newsfeed_1
 
         this.deadline = DateTime.fromISO(this.carbon.timestamp)
         this.feed =
@@ -167,6 +202,11 @@ export default {
 }
  */
 
+// Syntax helper for clamping a viewport unit to a min & max value
+@function fluid($smallest_size, $largest_size, $viewport_unit) {
+  @return max($smallest_size, min($largest_size, $viewport_unit));
+}
+
 $deadline: #eb1c23;
 $lifeline: #4aa1cc;
 
@@ -192,78 +232,147 @@ body {
 }
 $h: 45vh;
 $side: 5vmax;
-#deadline,
-#lifeline {
-  overflow: hidden;
-  padding-inline-start: $side;
-  color: black;
-  flex: 1 0 $h;
-  display: flex;
-  flex-wrap: wrap;
-  position: relative;
-  .group {
-    flex: 1 0 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    max-height: $h / 2;
+
+@mixin flavor($color) {
+  .groups {
+    background: $color;
   }
   .header {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: white;
-    position: absolute;
-  }
-  .value {
-    text-align: end;
-    //font-size: 14vh;
-    font-size: min(20vh, 20vw);
-    font-variant-numeric: tabular-nums;
-    margin: -3rem 0;
-  }
-  .label {
-    text-align: start;
-    font-size: 5vmax;
-    text-transform: uppercase;
-    position: relative;
-    top: 3vmax;
-  }
-  .side {
-    font-size: 4vh;
-    position: absolute;
-    //left: 1vmax;
-    left: 0;
-    padding-left: 1vmax;
-    top: 0;
-    bottom: 0;
-    background: black;
-    margin-inline-start: -1vmax;
-    width: $side;
-    display: flex;
-    flex-direction: column-reverse;
-    justify-content: center;
-    div {
-      text-align: center;
-      transform: rotate(-90deg);
+    color: $color;
+    .tab {
+      background-color: $color;
     }
   }
 }
-@mixin flavor($color) {
-  background: $color;
-  .side {
-    color: $color;
-  }
-}
-#lifeline {
-  @include flavor($lifeline);
+
+#deadline,
+#lifelines {
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+  color: black;
 }
 #deadline {
-  @include flavor($deadline);
+  .module {
+    @include flavor($deadline);
+    height: 100%;
+  }
+  .value {
+    font-size: fluid(3rem, 12rem, 15vw);
+    margin: -3rem 0;
+  }
+  .label {
+    font-size: fluid(2rem, 10rem, 6vw);
+    top: 3.75vh;
+  }
+  .group {
+    flex: 1 0 auto;
+  }
+}
+#lifelines {
+  flex: 1 1 20vh;
+  .module {
+    @include flavor($lifeline);
+    flex: 4 0 auto;
+  }
+  .value {
+    font-size: fluid(3rem, 10rem, 5vw);
+    align-items: flex-end;
+  }
+  .group {
+  }
+  .label {
+    font-size: 2vmax;
+    align-items: flex-start;
+  }
+}
+.module {
+  display: flex;
+  flex-direction: column;
+  .header {
+    font-size: fluid(1rem, 2rem, 3vw);
+    display: flex;
+    align-items: center;
+    flex: 0 0 4vh;
+    color: white;
+    background: black;
+    .tab {
+      display: flex;
+      align-items: center;
+      height: 100%;
+      color: black;
+      padding: 0 2%;
+      margin-inline-end: 2%;
+    }
+  }
+  .groups {
+    flex: 1 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    //height: 100%;
+    .group {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      max-height: $h / 2;
+    }
+    .value {
+      text-align: end;
+      font-variant-numeric: tabular-nums;
+    }
+    .label {
+      text-align: start;
+      text-transform: uppercase;
+      overflow-wrap: break-word;
+      position: relative;
+    }
+  }
+}
+#newsfeed {
+  position: relative;
+  min-height: 3rem;
+  .news {
+    color: $lifeline;
+    font-size: fluid(1rem, 2rem, 10vw);
+    text-transform: uppercase;
+    white-space: nowrap;
+    position: absolute;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    &[one] {
+      animation-name: widget-feed-one;
+    }
+    &[two] {
+      animation-name: widget-feed-two;
+    }
+  }
+}
+@keyframes widget-feed-one {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(-100%, 0);
+  }
+}
+@keyframes widget-feed-two {
+  0% {
+    transform: translate(100%, 0);
+  }
+  100% {
+    transform: translate(0%, 0);
+  }
 }
 #branding {
+  display: none;
+  background: black;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  height: 10vh;
+  /*
   display: flex;
-  flex: 1 0 100vh - 2 * $h;
+  flex: 0 0 10vh;
   justify-content: center;
   align-items: center;
   cursor: pointer;
@@ -281,5 +390,6 @@ $side: 5vmax;
       margin: 0;
     }
   }
+   */
 }
 </style>
